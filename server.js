e799031d3e515e8173eeb25a89a0315638927e0d
shradhaajr <jshradhaa@gmail.com>
2016-09-27 20:27:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,10 +25,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/a-one-style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'a-one-style.css'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
