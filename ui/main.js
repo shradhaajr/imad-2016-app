fc1c1b4ef7ce1728b23e7ba6d7ed1b4f1e2b1a3a
shradhaajr <jshradhaa@gmail.com>
2016-09-30 22:38:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
 
+//change the text of the main-text div
 var element = document.getElementById('main-text');
-
 element.innerHTML = 'This is being set by Javascript!';
+
+//move image on clicking
+var img = document.getElementById('madi');
+img.onclick = function (){
+  img.style.marginLeft = '100px';
+    
+};
