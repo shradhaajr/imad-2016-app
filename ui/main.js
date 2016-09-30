378264404c69968abbcbdca54593e15ef9ec17a7
shradhaajr <jshradhaa@gmail.com>
2016-09-30 22:42:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,10 @@ element.innerHTML = 'This is being set by Javascript!';
 
 //move image on clicking
 var img = document.getElementById('madi');
+function moveRight() {
+  marginLeft = marginLeft +10;
+  img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function (){
     var interval = setInterval(moveRight, 100);
 };
