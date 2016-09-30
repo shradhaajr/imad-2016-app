9c264eb6eb4807e958a4ae214f6fb8d86172ccc3
shradhaajr <jshradhaa@gmail.com>
2016-09-30 22:42:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ element.innerHTML = 'This is being set by Javascript!';
 
 //move image on clicking
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight() {
   marginLeft = marginLeft +10;
   img.style.marginLeft = marginLeft + 'px';
