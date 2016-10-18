bc11ac7ca4cd476869bbe4de8c2253dd3368e299
shradhaajr <jshradhaa@gmail.com>
2016-10-18 21:26:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,6 @@ button.onclick = function () {
 
 //submit name
 
-var nameInput= document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick= function() {
     //make a request to the server and send the name
@@ -58,6 +56,8 @@ submit.onclick= function() {
     };
     
     //make request
+    var nameInput= document.getElementById('name');
+    var name=nameInput.value;
     request.open('GET', 'http://shradhaajr.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
