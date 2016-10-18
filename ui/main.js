d1e206f8edcca156d003d98ac4273c438dce6d4f
shradhaajr <jshradhaa@gmail.com>
2016-10-18 21:22:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,8 @@ submit.onclick= function() {
             //take some action
             if(request.status == 200) {
                 //capture a list of names and render it as a list
-                var names= ['name1', 'name2', 'name3', 'name4'];
+                var names= request.responseText;
+                names=JSON.parse(names);
                 var list='';
                 
                 for(var i=0;i<names.length; i++)
