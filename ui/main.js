819213594cbf4dc2496b8eb69262871cfa8aa7fa
shradhaajr <jshradhaa@gmail.com>
2016-10-21 22:40:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,5 @@
 console.log('Loaded!');
 
-var button = document.getElementById("counter");
-button.onclick = function () {
-    
-    //create request
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it variable
-    request.onreadystatechange = function () {
-        if(request.readyState == XMLHttpRequest.DONE) {
-            //take some action
-            if(request.status == 200) {
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    //make request
-    request.open('GET', 'http://shradhaajr.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
-    
-};
-
 //submit name
 
 var submit=document.getElementById('submit_btn');
