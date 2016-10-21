faec1f2e708c689f98060d3083787d1d017736b6
shradhaajr <jshradhaa@gmail.com>
2016-10-21 23:06:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,6 +132,7 @@ function createTemplate (data) {
                 </div>
             </div>
             <div class="comment center">
+                <input type="text" id="name" placeholder="name">
                 <textarea id="comment" placeholder="share your opinion!"></textarea>
                  <br/>
                 <input type="submit" value="Submit" id="submit_btn">
