48c424d32fd904c7e789bfb1f4f39105670ae7f3
shradhaajr <jshradhaa@gmail.com>
2016-10-29 11:07:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ app.get('/submit-comment', function(req, res) { //URL: /submit-name?name=xxxx
     res.send(JSON.stringify(comments));
 });
 
-app.get('/poems:poemName', function(req, res){
+app.get('/poems/:poemName', function(req, res){
     //poemName == poem-one
     //poems[poemName] == {} content object for article-one
     var poemName = req.params.poemName;
@@ -223,7 +223,7 @@ app.get('/poems:poemName', function(req, res){
         }
         else{
             if(result.rows.length === 0){
-                res.status(404).send('Article not found');
+                res.status(404).send('Poem not found');
             }
             else{
                 var poemData = result.rows[0];
