f887e06b0926751676c590276b441271d0190a63
shradhaajr <jshradhaa@gmail.com>
2016-10-29 11:51:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ app.get('/:poemName', function(req, res){
     //poems[poemName] == {} content object for article-one
     var poemName = req.params.poemName;
     
-    pool.query("SELECT * FROM poem WHERE varname = '"+ req.params.poemName + "'", function(err, result) {
+    pool.query("SELECT * FROM test", function(err, result) {
         if(err){
             res.status(500).send(err.toString());
         }
