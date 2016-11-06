a684e8bf33aea46f83317667f0cb8c35d3fcfa7e
shradhaajr <jshradhaa@gmail.com>
2016-11-06 12:38:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,5 @@
 console.log('Loaded!');
 
-//submit username/password
-var submit=document.getElementById('submit_btn_login');
-submit.onclick= function() {
-    //make a request to the server and send the name
-    
-     //create request
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it variable
-    request.onreadystatechange = function () {
-        if(request.readyState == XMLHttpRequest.DONE) {
-            //take some action
-            if(request.status == 200) {
-                console.log('user logged in');
-                alert('loged in successfully');
-            }else if(request.status === 403) {
-                alert('username/password is incorrect');
-            }else if(request.status === 500){
-                alert('something went wrong on the server');
-            }
-        }
-    };
-    
-    //make request
-    var username= document.getElementById('username').value;
-    var password= document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    request.open('POST', 'http://shradhaajr.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username: username, password: password}));
-};
-
-//--------------------------------------------------------------------------------------
-
 //submit comment
 
 var submit=document.getElementById('submit_btn');
@@ -73,3 +38,40 @@ submit.onclick= function() {
     request.send(null);
     commentInput.value="";
 };
+
+//------------------------------------------------------------------------------
+//submit username/password
+var submit=document.getElementById('submit_btn_login');
+submit.onclick= function() {
+    //make a request to the server and send the name
+    
+     //create request
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it variable
+    request.onreadystatechange = function () {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status == 200) {
+                console.log('user logged in');
+                alert('loged in successfully');
+            }else if(request.status === 403) {
+                alert('username/password is incorrect');
+            }else if(request.status === 500){
+                alert('something went wrong on the server');
+            }
+        }
+    };
+    
+    //make request
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://shradhaajr.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
+//--------------------------------------------------------------------------------------
+
