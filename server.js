063a41af937f7cad318eb32127131592511420ca
shradhaajr <jshradhaa@gmail.com>
2016-11-06 18:23:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,26 @@ app.post('/login', function (req, res) {
    });
 });
 
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('Successfully logged out');
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     //make a select request
