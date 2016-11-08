955bab34d11c3ec13112a2605119f74b6454ac3a
shradhaajr <jshradhaa@gmail.com>
2016-11-08 23:02:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,41 +1,116 @@
 console.log('Loaded!');
 
 //------------------------------------------------------------------------------
-//submit username/password
-var login=document.getElementById('submit_btn_login');
-login.onclick= function() {
-    //make a request to the server and send the name
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to join the poesy!</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
     
-     //create request
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+}
+
+function loadLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
-    
-    //capture the response and store it variable
     request.onreadystatechange = function () {
-        if(request.readyState == XMLHttpRequest.DONE) {
-            //take some action
-            if(request.status == 200) {
-                console.log('user logged in');
-                alert('loged in successfully');
-            }else if(request.status === 403) {
-                alert('username/password is incorrect');
-            }else if(request.status === 500){
-                alert('something went wrong on the server');
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
     };
     
-    //make request
-    var username= document.getElementById('username').value;
-    var password= document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    request.open('POST', 'http://shradhaajr.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username: username, password: password}));
-};
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+loadLogin();
 
 //--------------------------------------------------------------------------------------
-
+/*
 //submit comment
 
 var submit=document.getElementById('submit_btn');
@@ -74,4 +149,4 @@ submit.onclick= function() {
     request.send(null);
     commentInput.value="";
 };
-
+*/
