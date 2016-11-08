498056b5f85e3cd5615206ea634c84ff3e6a44c6
shradhaajr <jshradhaa@gmail.com>
2016-11-09 00:32:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -115,11 +115,12 @@ function loadPoems () {
         if (request.readyState === XMLHttpRequest.DONE) {
             var poems = document.getElementById('poems');
             if (request.status === 200) {
-                var content = '';
+                var content = '<div class="content"';
                 var poemData = JSON.parse(this.responseText);
                 for (var i=0; i< poemData.length; i++) {
                     content += `<a href="/poems/${poemData[i].varname}" style="text-decoration:none;">${poemData[i].heading}</a>(${poemData[i].date.split('T')[0]})<br/>`;
                 }
+                content+='</div>';
                 poems.innerHTML = content;
             } else {
                 poems.innerHTML('Oops! Could not load all articles!');
